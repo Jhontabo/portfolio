72dde50f3ae439622385614a5a4e8707e9932b8b
Jhontabo <jhonse.tajumbina@gmail.com>
2026-09-23 18:44:00 -0500

    Use uc export view URL to avoid Google Drive thumbnail rate limit

--- lib/drive.ts
+++ lib/drive.ts
@@ -1,6 +1,6 @@
 export function getDriveImageUrl(link: string): string | null {
   const match = link.match(/\/file\/d\/([^/?#]+)/);
-  if (match) return `https://drive.google.com/thumbnail?id=${match[1]}&sz=w800`;
+  if (match) return `https://drive.google.com/uc?export=view&id=${match[1]}`;
   if (link.includes("drive.google.com") && link.includes("/uc?")) return link;
   return link;
 }
